def19fa96ffbd51320da3019e39a43dfe4f08423
QuaRang <dbduddnd1225@gmail.com>
2024-04-04 16:26:52 +0900

    피로도 문제 풀이 재시도

--- 피로도.playground/Contents.swift
+++ 피로도.playground/Contents.swift
@@ -2,21 +2,20 @@ import UIKit
 
 
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
-    var visited = [Bool](repeating: false, count: dungeons.count)
-    var result = 0
     
-    func dfs(_ fatigue: Int,_ cnt:Int) {
-        result = max(result, cnt)
-        (0..<dungeons.count).forEach{ i in
-            if  !visited[i] && fatigue >= dungeons[i][0]{
-                visited[i] = true
-                dfs(fatigue - dungeons[i][1],cnt + 1)
-                visited[i] = false
+    var result = 0
+    var visited = [Bool](repeating: false, count: dungeons.count)
+    func dfs(pirodo:Int,cnt:Int){
+        result = max(result,cnt)
+        for i in (0..<dungeons.count){
+            if !visited[i],dungeons[i][0] <= pirodo{
+                visited[i] = true   //특정 인덱스 검사
+                dfs(pirodo: pirodo - dungeons[i][1], cnt: cnt + 1)
+                visited[i] = false  //검사 완료되고 다른 케이스를 검사하기 위함
             }
         }
     }
-    
-    dfs(k,result)
+    dfs(pirodo: k, cnt: 0)
     return result
 }
 
